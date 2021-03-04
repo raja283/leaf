f97813ab0ac59be3334a3ee252429a689f1065c0
Dell <Dell@DESKTOP-MNQGH03>
2021-03-04 19:39:16 +0530

    initial commit

---
+++ feat/src/main/java/cucu/feat/App.java
@@ -0,0 +1,13 @@
+package cucu.feat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ feat/src/main/java/cucu/feat/testrun.java
@@ -0,0 +1,15 @@
+package cucu.feat;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="Feature",
+		glue="stepDefinitions"
+		)
+public class testrun {
+
+}
